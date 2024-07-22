cddb978bb4982b7839c747f4595748157e14f5b2
rakib-shahid <rakibshahid1298@gmail.com>
2024-07-22 19:45:28 -0400

    latest

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -4,7 +4,6 @@
 // CHANGE THIS TO YOUR ACTUAL GIF NAME
 // MAKE YOUR GIF NAME ONLY ALPHANUMERIC
 
-
 #include QMK_KEYBOARD_H
 #include <qp.h>
 // #include "gifs/sanacut.qgf.h"
@@ -18,88 +17,79 @@
 #define DEBUG_ENABLED false
 // #define DEBUG_ENABLED true
 
-
-
-static char textArr[31];
-static char songArr[31];
+static char    textArr[31];
+static char    songArr[31];
 static uint8_t image_data[32768];
-static int image_counter = 0;
+static int     image_counter = 0;
 
-enum layer_names{
-    _NUMPAD,
-    _MEDIA,
-    _LAYER3,
-    _LAYER4
-};
+enum layer_names { _NUMPAD, _MEDIA, _LAYER3, _LAYER4 };
 
 // static char test[] = "Initial";
 
-// uint8_t framebuffer[SURFACE_REQUIRED_BUFFER_BYTE_SIZE(WIDTH, HEIGHT, 16)] = {0}; // this is where your image data is stored 
+// uint8_t framebuffer[SURFACE_REQUIRED_BUFFER_BYTE_SIZE(WIDTH, HEIGHT, 16)] = {0}; // this is where your image data is stored
 // painter_device_t surface = qp_make_rgb565_surface(WIDTH, HEIGHT, (void *)framebuffer);
 
-static painter_device_t display = NULL;
-static painter_image_handle_t image = NULL;
-static painter_font_handle_t my_font;
-static deferred_token my_anim;
-static bool animating = false;
-static bool layer_name_displayed = false;
-static int messages_since_layer_state = 0;
+static painter_device_t       display = NULL;
+static painter_image_handle_t image   = NULL;
+static painter_font_handle_t  my_font;
+static deferred_token         my_anim;
+static bool                   animating                  = false;
+static bool                   layer_name_displayed       = false;
+static int                    messages_since_layer_state = 0;
 // static bool special_anim = false;
 static bool album_art = false;
 static bool timed_out = false;
 static bool caps_on;
 // static int playing = 0;
 
-void turn_off_screen(void){
+void turn_off_screen(void) {
     setPinOutput(GP29);
     writePinLow(GP29);
 }
 
-void turn_on_screen(void){
+void turn_on_screen(void) {
     setPinOutput(GP29);
     writePinHigh(GP29);
 }
 
-void wipe_image(void){
-    qp_rect(display, 0,0,130, 130, HSV_BLACK, true);
+void wipe_image(void) {
+    qp_rect(display, 0, 0, 130, 130, HSV_BLACK, true);
 }
 
-uint8_t* doubleArray(uint8_t* originalArray, int originalSize){
-    uint8_t* newArr = malloc(2*originalSize*sizeof(uint8_t));
-    for (int i = 0; i < originalSize; i+=2){
-        newArr[i*2]     = originalArray[i];
-        newArr[i*2+1]   = originalArray[i+1];
-        newArr[i*2+2]   = originalArray[i];
-        newArr[i*2+3]   = originalArray[i+1];
+uint8_t *doubleArray(uint8_t *originalArray, int originalSize) {
+    uint8_t *newArr = malloc(2 * originalSize * sizeof(uint8_t));
+    for (int i = 0; i < originalSize; i += 2) {
+        newArr[i * 2]     = originalArray[i];
+        newArr[i * 2 + 1] = originalArray[i + 1];
+        newArr[i * 2 + 2] = originalArray[i];
+        newArr[i * 2 + 3] = originalArray[i + 1];
     }
     return newArr;
 }
-
 
 // hid function
 void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
     switch (data[0]) {
         // new song string
-        case 0xFF: 
+        case 0xFF:
             // uprintf("New song string received\n");
-            if (data[1]){
+            if (data[1]) {
                 album_art = true;
                 messages_since_layer_state += 1;
-                if (animating){
+                if (animating) {
                     animating = false;
                     qp_stop_animation(my_anim);
                 }
-                if (((strncmp(songArr,(char *)(data+2),strlen(songArr)-1) != 0) || layer_name_displayed) && messages_since_layer_state > 3){
-                    qp_rect(display, 0,132,131, 162, HSV_BLACK, true);
-                    qp_drawtext(display, 2, 138, my_font, (char *)(data+2));
-                    strcpy(songArr,(char *)(data+2));
+                if (((strncmp(songArr, (char *)(data + 2), strlen(songArr) - 1) != 0) || layer_name_displayed) && messages_since_layer_state > 3) {
+                    qp_rect(display, 0, 132, 131, 162, HSV_BLACK, true);
+                    qp_drawtext(display, 2, 138, my_font, (char *)(data + 2));
+                    strcpy(songArr, (char *)(data + 2));
                     layer_name_displayed = false;
                 }
-            }
-            else {
-                strcpy(songArr,"\0");
+            } else {
+                strcpy(songArr, "\0");
                 // wipe_image();
-                qp_rect(display, 0,130,130, 162, HSV_BLACK, true);
+                qp_rect(display, 0, 130, 130, 162, HSV_BLACK, true);
                 // return;
                 album_art = false;
             }
@@ -107,7 +97,7 @@ void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
 
         // new image data (1st hid message)
         case 0xED:
-            uprintf("New image received\n");
+            uprintf("New image received, ");
             memset(image_data, 0, sizeof(image_data));
             image_counter = 0;
             if (animating) {
@@ -121,21 +111,21 @@ void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
             //     uprintf("%d,", image_data[i]);
             // }
             // uprintf("\n");
-            image_counter += 30;
+            image_counter += 1;
             // uprintf("Initial image data copied, counter: %d\n", image_counter);
             break;
 
         // intermediate image data
         case 0xEE:
             // uprintf("Intermediate image data received\n");
-            memcpy(image_data + image_counter, data + 1, 30);
-            image_counter += 30;
+            memcpy(image_data + (image_counter * 30), data + 1, 30);
+            image_counter += 1;
             // uprintf("Intermediate image data copied, counter: %d\n", image_counter);
             break;
 
         // final image data
         case 0xFC:
-            uprintf("Final image data received\n");
+
             // // print size of image_data
             // uprintf("Size of image_data: %d\n", sizeof(image_data));
             // // print last 30 elements of image_data
@@ -145,9 +135,10 @@ void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
             // }
             // uprintf("\n");
             wipe_image();
-            memcpy(image_data + image_counter, data + 1, 30);
+            memcpy(image_data + (image_counter * 30), data + 1, 30);
+            image_counter += 1;
             // print image counter
-            // uprintf("Image counter: %d\n",image_counter);
+            uprintf("Image counter: %d\n", image_counter);
             // uint8_t* pixels = doubleArray(image_data, 32768); // Update to match the new array size
             for (int i = 0, c = 0; i < 128; i += 1, c += 1) {
                 // draw to 2 columns
@@ -199,60 +190,59 @@ void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
     }
 }
 // bool timeout_task(bool &album_art, bool &timed_out, bool &animating, int activity){
-//     if 
+//     if
 // }
 
-void writeLayerState(char* toWrite){
-    qp_rect(display, 0,132,131, 162, HSV_BLACK, true);
+void writeLayerState(char *toWrite) {
+    qp_rect(display, 0, 132, 131, 162, HSV_BLACK, true);
     qp_drawtext(display, 2, 138, my_font, (char *)(toWrite));
 }
 
-void housekeeping_task_user(void){
+void housekeeping_task_user(void) {
     int last_activity = last_input_activity_elapsed();
     rgblight_set();
     // optionally last_encoder_activity_elapsed() instead
-    if (!album_art){
-        if (!timed_out){
+    if (!album_art) {
+        if (!timed_out) {
             // timeout screen no activity
-            if (last_activity > 30000){
+            if (last_activity > 30000) {
                 turn_off_screen();
                 timed_out = true;
-                if (animating){
+                if (animating) {
                     qp_stop_animation(my_anim);
                     animating = false;
                 }
             }
             // disable animation for encoder
-            else if (last_activity < 1000){
-                if (animating){
+            else if (last_activity < 1000) {
+                if (animating) {
                     qp_stop_animation(my_anim);
                     wipe_image();
                     // possibly do layer stuff here
                     animating = false;
                 }
             }
-            // turn animation back on 
-            else if (!animating){
-                my_anim = qp_animate(display,0,0,image);
+            // turn animation back on
+            else if (!animating) {
+                my_anim   = qp_animate(display, 0, 0, image);
                 animating = true;
             }
 
         }
         // turn screen back on after timeout
         else {
-            if (last_activity < 30000){
+            if (last_activity < 30000) {
                 turn_on_screen();
-                if (!animating){
-                    my_anim = qp_animate(display,0,0,image);
+                if (!animating) {
+                    my_anim   = qp_animate(display, 0, 0, image);
                     animating = true;
                 }
                 timed_out = false;
             }
         }
-    }
-    else {
+    } else {
         // keep screen on and do album stuff
-        if (timed_out){
+        if (timed_out) {
             turn_on_screen();
             timed_out = false;
         }
@@ -260,36 +250,36 @@ void housekeeping_task_user(void){
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    caps_on = host_keyboard_led_state().caps_lock;
+    caps_on                    = host_keyboard_led_state().caps_lock;
     messages_since_layer_state = 0;
-    switch (get_highest_layer(state)){
-        case    _NUMPAD:
+    switch (get_highest_layer(state)) {
+        case _NUMPAD:
             writeLayerState("Numpad Layer");
             if (!caps_on) {
-                sethsv(127,255,30, (rgb_led_t *)&led[0]);
+                sethsv(127, 255, 30, (rgb_led_t *)&led[0]);
             }
             break;
-        case    _MEDIA:
+        case _MEDIA:
             writeLayerState("Media Layer");
             if (!caps_on) {
-                sethsv(201,255,30, (rgb_led_t *)&led[0]);
+                sethsv(201, 255, 30, (rgb_led_t *)&led[0]);
             }
             break;
-        case    _LAYER3:
+        case _LAYER3:
             writeLayerState("Navigation Layer");
             if (!caps_on) {
-                sethsv(85,255,30, (rgb_led_t *)&led[0]);
+                sethsv(85, 255, 30, (rgb_led_t *)&led[0]);
             }
             break;
-        case    _LAYER4:
+        case _LAYER4:
             writeLayerState("Macro Layer");
             if (!caps_on) {
-                sethsv(21,255,30, (rgb_led_t *)(&led[0]));
+                sethsv(21, 255, 30, (rgb_led_t *)(&led[0]));
             }
             break;
     }
     layer_name_displayed = true;
-  return state;
+    return state;
 }
 
 __attribute__((weak)) bool qp_st7735_init(painter_device_t device, painter_rotation_t rotation) {
@@ -330,24 +320,24 @@ void keyboard_post_init_user(void) {
     setPinInputHigh(GP22);
     setPinInputHigh(GP21);
     debug_enable = DEBUG_ENABLED;
-    
+
     // create display
     display = qp_st7735_make_spi_device(130, 161, GP18, GP16, GP17, 0, 3);
     qp_init(display, QP_ROTATION_180);
-    qp_rect(display, 0,0,130, 161, HSV_BLACK, true);
+    qp_rect(display, 0, 0, 130, 161, HSV_BLACK, true);
 
     // load image
     image = qp_load_image_mem(gfx_vapor);
-    if (image != NULL){
-        my_anim = qp_animate(display,0,0,image);
+    if (image != NULL) {
+        my_anim   = qp_animate(display, 0, 0, image);
         animating = true;
     }
 
     // load font
     my_font = qp_load_font_mem(font_scp);
-    qp_drawtext(display, 2, 138, my_font, textArr);    
-    
-    sethsv(0,0,0, (rgb_led_t *)&led[0]);
+    qp_drawtext(display, 2, 138, my_font, textArr);
+
+    sethsv(0, 0, 0, (rgb_led_t *)&led[0]);
 }
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
@@ -369,39 +359,13 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 //     return true;
 // }
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     //  NUMPAD
-    [0] = LAYOUT_ortho_5x4(
-        KC_HOME,    KC_END,     KC_DELETE,  KC_MUTE,
-        KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_KP_MINUS,
-        KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_KP_PLUS,
-        KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_KP_ASTERISK,
-        TO(1),      KC_KP_0,    KC_KP_DOT,  KC_KP_SLASH
-    ),
+    [0] = LAYOUT_ortho_5x4(KC_HOME, KC_END, KC_DELETE, KC_MUTE, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_MINUS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ASTERISK, TO(1), KC_KP_0, KC_KP_DOT, KC_KP_SLASH),
     // MEDIA KEYS
-    [1] = LAYOUT_ortho_5x4(
-        KC_HOME,    KC_END,     KC_DELETE,  KC_MUTE,
-        KC_FIND,                KC_CAPS_LOCK,                KC_WH_U,                KC_AUDIO_MUTE,
-        KC_MEDIA_PREV_TRACK,    KC_MPLY,    KC_MEDIA_NEXT_TRACK,    KC_AUDIO_VOL_UP,
-        KC_CALC,                KC_MYCM,                KC_WH_D,                KC_AUDIO_VOL_DOWN,
-        TO(2),                  KC_CUT,                 KC_COPY,                KC_PASTE
-    ),
+    [1] = LAYOUT_ortho_5x4(KC_HOME, KC_END, KC_DELETE, KC_MUTE, KC_FIND, KC_CAPS_LOCK, KC_WH_U, KC_AUDIO_MUTE, KC_MEDIA_PREV_TRACK, KC_MPLY, KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_UP, KC_CALC, KC_MYCM, KC_WH_D, KC_AUDIO_VOL_DOWN, TO(2), KC_CUT, KC_COPY, KC_PASTE),
     // NAVIGATION KEYS
-    [2] = LAYOUT_ortho_5x4(
-        KC_HOME,    KC_END,     KC_DELETE,  KC_MUTE,
-        KC_INSERT,      KC_HOME,    KC_PGUP,        KC_PSCR,
-        KC_DELETE,      KC_END,     KC_PGDN,        KC_SCROLL_LOCK,
-        KC_COPY,        KC_PASTE,    KC_UP,          KC_PAUSE,
-        TO(3),          KC_LEFT,    KC_DOWN,        KC_RIGHT
-    ),
+    [2] = LAYOUT_ortho_5x4(KC_HOME, KC_END, KC_DELETE, KC_MUTE, KC_INSERT, KC_HOME, KC_PGUP, KC_PSCR, KC_DELETE, KC_END, KC_PGDN, KC_SCROLL_LOCK, KC_COPY, KC_PASTE, KC_UP, KC_PAUSE, TO(3), KC_LEFT, KC_DOWN, KC_RIGHT),
     // blank for now
-    [3] = LAYOUT_ortho_5x4(
-        KC_HOME,    KC_END,     KC_DELETE,  KC_MUTE,
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO ,     KC_NO,      KC_NO,      KC_NO,
-        TO(0),      KC_NO,      KC_NO,      KC_NO
-    )
-};
+    [3] = LAYOUT_ortho_5x4(KC_HOME, KC_END, KC_DELETE, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO)};
